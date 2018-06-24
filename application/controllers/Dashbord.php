8496a0654375f3b67ba8fb6705452123b4dbd026
faizal97 <faizalardianputra@yahoo.co.id>
2018-06-24 08:09:55 +0700

    update dasboard

--- application/controllers/Dashbord.php
+++ application/controllers/Dashbord.php
@@ -16,8 +16,13 @@ class Dashbord extends CI_Controller {
 	public function index()
 	{
 		$this->load->library('session');
+		$user = $this->session->id_pelajar;
 		if(isset($this->session->id_pelajar)){
+			$query = $this->db->query("SELECT * FROM tb_kursus INNER JOIN tb_pengajar ON tb_kursus.id_pengajar = tb_pengajar.id_pengajar INNER JOIN tb_pelajar ON tb_pengajar.id_pelajar = tb_pelajar.id_pelajar INNER JOIN tb_kategori ON tb_kursus.id_kategori = tb_kategori.id_kategori ORDER BY RAND() LIMIT 4 ");
+			$query2 = $this->db->query("SELECT * FROM tb_kursus INNER JOIN tb_pengajar ON tb_kursus.id_pengajar = tb_pengajar.id_pengajar INNER JOIN tb_pelajar ON tb_pengajar.id_pelajar = tb_pelajar.id_pelajar INNER JOIN tb_kategori ON tb_kursus.id_kategori = tb_kategori.id_kategori ORDER BY tb_kursus.tgl_buat DESC LIMIT 4 ");
 			$data = array(
+				'data_kursus' => $query,
+				'data_kursus_baru' => $query2,
 				'nama_depan' => ucwords($this->session->userdata['nama_depan']),
 				'nama_belakang' => $this->session->userdata['nama_belakang'],
 				'title' => "Yuk Bali - Home"
--- application/views/dashbord.php
+++ application/views/dashbord.php
@@ -1,4 +1,36 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>css/dashbord.css">
 <div class="container-fluid">
-	
+	<h1>Jelajah Kursus</h1>
+	<div class="row">
+	<?php foreach ($data_kursus->result() as $row) {?>
+		<div class="col-sm-3">
+		<div class="card">
+         <img class="card-img-top" src="<?php echo base_url($row->gambar_kursus) ?>" width="300px" height="250px" alt="Card Image">
+         <div class="card-body">
+             <h5 class="card-title"><?php echo $row->nama_kursus ?></h5>
+			 <p style='line-height:0px;'><?php echo $row->nama_kategori ?></p>
+			 <p style='line-height:10px;'><?php echo $row->nama_depan." ".$row->nama_belakang ?></p>
+			 <button class="btn btn-success form-control">DAFTAR</button>
+         </div>
+        </div>
+		</div>
+	<?php } ?>
+	</div>
+	<br>
+	<h1>Kursus Terbaru</h1>
+	<div class="row">
+	<?php foreach ($data_kursus_baru->result() as $row) {?>
+		<div class="col-sm-3">
+		<div class="card">
+         <img class="card-img-top" src="<?php echo base_url($row->gambar_kursus) ?>" width="300px" height="250px" alt="Card Image">
+         <div class="card-body">
+             <h5 class="card-title"><?php echo $row->nama_kursus ?></h5>
+			 <p style='line-height:0px;'><?php echo $row->nama_kategori ?></p>
+			 <p style='line-height:10px;'><?php echo $row->nama_depan." ".$row->nama_belakang ?></p>
+			 <button class="btn btn-success form-control">DAFTAR</button>
+         </div>
+        </div>
+		</div>
+	<?php } ?>
+	</div>
 </div>
