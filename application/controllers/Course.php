558d8d4ff4797b44e397c986ec40af590c0d1339
faizal97 <faizalardianputra@yahoo.co.id>
2018-06-27 14:07:31 +0700

    update database

--- application/controllers/Course.php
+++ application/controllers/Course.php
@@ -22,17 +22,33 @@ class Course extends CI_Controller {
 		$query = $this->db->query("SELECT * FROM tb_kursus INNER JOIN tb_kategori ON tb_kursus.id_kategori = tb_kategori.id_kategori INNER JOIN tb_pengajar ON tb_kursus.id_pengajar = tb_pengajar.id_pengajar INNER JOIN tb_pelajar ON tb_pelajar.id_pelajar = tb_pengajar.id_pelajar WHERE tb_kursus.nama_kursus='$nama_kursus2' AND tb_pelajar.nama_depan='$nama_depan' AND tb_pelajar.nama_belakang='$nama_belakang'");
 		$query = $query->row();
 		$id_kursus = $query->id_kursus;
+		$id_pengajar = $query->id_pengajar;
 		$data_materi = $this->db->query("SELECT * FROM tb_materi WHERE id_kursus='$id_kursus'");
 		$jumlah_materi = $data_materi->num_rows();
+		$data_kursus_pengajar = $this->db->query("SELECT * FROM tb_kursus WHERE id_pengajar='$id_pengajar'");
+		$jumlah_kursus_pengajar = $data_kursus_pengajar->num_rows();
+		$data_materi = $this->db->query("SELECT * FROM tb_materi WHERE id_kursus='$id_kursus'");
 		$data = [
+			'data_materi' => $data_materi,
 			'data_kursus' => $query,
 			'nama_pengajar' => $nama_pengajar2,
 			'nama_kursus' => $nama_kursus2,
 			'jumlah_materi' => $jumlah_materi,
+			'jumlah_kursus_pengajar' => $jumlah_kursus_pengajar
 		];
 		$this->template->header($nama_kursus2,2);
 		$this->load->view('detail_kursus', $data);
 		$this->template->footer();
 		
+	}
+
+	public function daftar_kursus($id_kursus)
+	{
+		$user = $this->session->id_pelajar;
+		$data = [
+			'id_pelajar' => $user,
+			'id_kursus' => $id_kursus,
+		];
+		$result = $this->db->insert('tb_detail_kursus',$data);
 	}
 }
--- application/views/detail_kursus.php
+++ application/views/detail_kursus.php
@@ -4,11 +4,13 @@
      <a class="breadcrumb-item" href="<?php echo base_url('home.html') ?>">Beranda</a>
      <span class="breadcrumb-item active"><?php echo $nama_kursus ?></span>
  </nav>
+ </div>
+
 <div class="container" style="width: 100%">
-    
+<a href="<?php echo base_url('home.html') ?>" class="btn btn-primary">Kembali</a>
 	<div class="card border-info mb-3">
 		<div class="card-header">
-			<h2>Detail Kursus</h2>
+			<h2><?php echo $nama_kursus ?></h2>
 		</div>
 		<div class="card-body">
 			<div class="row">
@@ -19,12 +21,44 @@
 					<label>Nama Kursus : <?php echo $nama_kursus ?></label><br>
 					<label>Kategori : <?php echo $data_kursus->nama_kategori ?></label><br>
 					<label>Jumlah Materi : <?php echo $jumlah_materi ?></label><br>
-					<label>Nama Pengajar : <?php echo $nama_pengajar ?></label>
+					<label>Nama Pengajar : <?php echo $nama_pengajar ?></label><br>
+					<a href="" class="form-control btn btn-primary" style="width:80%">DAFTAR</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12">
+					<?php echo $data_kursus->deskripsi_kursus ?>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-sm-4 mt-5">
 					<img src="<?php echo base_url($data_kursus->foto_profil)  ?>" width="300" height="300" alt="">
+				</div>
+				<div class="col-sm-8 mt-5">
+				<h1><?php echo $nama_pengajar ?></h1>
+				<h4><?php echo $jumlah_kursus_pengajar ?> Kursus</h4>
+					<?php echo $data_kursus->deskripsi_pengajar ?>
+				</div>
+			</div>
+			<div class="row mt-5">
+				<div class="col-sm-12">
+				<h1>Materi Kursus</h1>
+					<table class="table table-striped" style="width:80%;" align="center">
+					<thead class="thead-dark">
+						<tr>
+							<td scope="col" class="text-center">No. Materi</td>
+							<td scope="col">Nama Materi</td>
+						</tr>
+					</thead>
+					<tbody>
+						<?php foreach ($data_materi->result() as $row) {?>
+						<tr>
+						<td class="text-center" scope="row"><?php echo $row->urut ?></td>
+						<td><?php echo $row->nama_materi ?></td>
+						</tr>
+						<?php } ?>
+						</tbody>
+					</table>
 				</div>
 			</div>
 		</div>
