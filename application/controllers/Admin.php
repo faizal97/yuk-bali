01d4625623df24a6234a02b260c421c88b3f1c67
faizal97 <faizalardianputra@yahoo.co.id>
2018-06-30 18:52:49 +0700

    update jumlah data di home admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,7 +6,19 @@ class Admin extends CI_Controller {
 
     public function index()
     {
-    	$data = array();
+			$data_pelajar = $this->db->query("SELECT * FROM tb_pelajar");
+			$jumlah_pelajar = $data_pelajar->num_rows();
+
+			$data_pengajar = $this->db->query("SELECT * FROM tb_pengajar");
+			$jumlah_pengajar = $data_pengajar->num_rows();
+
+			$data_kursus = $this->db->query("SELECT * FROM tb_kursus");
+			$jumlah_kursus = $data_kursus->num_rows();
+    	$data = array(
+			'jumlah_pelajar' => $jumlah_pelajar,
+			'jumlah_pengajar' => $jumlah_pengajar,
+			'jumlah_kursus' => $jumlah_kursus,
+		);
     	$this->load->view('settings/bootstrap',$data);
     	$this->load->view('admin/menu.php');
         $this->load->view('admin/home.php');
--- application/views/admin/home.php
+++ application/views/admin/home.php
@@ -4,19 +4,19 @@
         <div id="kotak1">
             <img id="logokotak1" src="<?php echo base_url('img/beranda/decul.png') ?>" >
             <h4> Data Pelajar</h4>
-            <label id="ket"> Mempunyai Data Sebanyak : 00 Data Pelajar</label>
+            <label id="ket"> Mempunyai Data Sebanyak : <?php echo $jumlah_pelajar ?> Data Pelajar</label>
         </div>
         <div id="kotak2" style="margin-left:470px">
             <img id="logokotak2" src="<?php echo base_url('img/beranda/pengajaar.png') ?>" >
             <h4> Data Pengajar</h4>
-            <label id="ket"> Mempunyai Data Sebanyak : 00 Data Pengajar</label>
+            <label id="ket"> Mempunyai Data Sebanyak : <?php echo $jumlah_pengajar ?> Data Pengajar</label>
         </div>
     </div>
     <div class="samping" style="margin-top:370px">
         <div id="kotak3">
             <img id="logokotak3" src="<?php echo base_url('img/beranda/laporan.png') ?>" >
             <h4> Data Kursus</h4>
-            <label id="ket"> Mempunyai Data Sebanyak : 00 Data Kursus</label>
+            <label id="ket"> Mempunyai Data Sebanyak : <?php echo $jumlah_kursus ?> Data Kursus</label>
         </div>
         <div id="kotak4" style="margin-left:470px">
             <img id="logokotak4" src="<?php echo base_url('img/beranda/laporan.png') ?>" >
