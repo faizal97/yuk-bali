9df659366da6ad1c41261ec7d82104e635671a85
aldhan73 <aldhanernest73@gmail.com>
2018-07-20 16:55:59 +0700

    udh bner sal logonya wkwk

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/footer.css') ?>">
 <div>
 <footer id="footer">
-	<img id="logo" src="<?php echo base_url('img/futer1.png')?>">
+	<img id="logofuter" src="<?php echo base_url('img/futer1.png')?>">
 </footer>
 </div>
 </body>
